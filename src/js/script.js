92cc2170ca7e63eb9f558958a9f221ba4d8d3924
Cristian Felipe <cristianpipe0494@gmail.com>
2018-11-29 17:58:06 -0600

    Have added the basic functionality to the Main Object in the game.

--- src/js/script.js
+++ src/js/script.js
@@ -1,36 +1,61 @@
-// Get the elements from the DOM.
+//Get the elements from the DOM.
 const canvasArea = document.getElementById('canvasArea');
 
-// Set the Canvas Width and Height as the Window.
+//Set the Canvas Width and Height as the Window.
 canvasArea.width = window.innerWidth;
 canvasArea.height = window.innerHeight;
 
 // Create a context for the Canvas Field.
 // Canvas context is 2d.
-let canvasContext = canvasArea.getContext('2d');
+const canvasContext = canvasArea.getContext('2d');
 
 // Main Character.
 // Variables for the Character.
-let x =  innerWidth / 2;
-let y = innerHeight / 1.5;
-let startPoint = 0;
-let endPoint = Math.PI * 2;
-let radius = 30;
-let xVelocity = 0;
+const x = canvasArea.width / 2;
+const y = canvasArea.height / 1.5;
+const startPoint = 0;
+const endPoint = Math.PI * 2;
+const radius = 50;
+const xRightVelocity = 0;
+const xLeftVelocity = 0;
+const color = 'red';
+
+function MainCharacter(xKey, yKey, radiusKey, startPointKey, endPointKey, xLeftVelocityKey, xRightVelocityKey, colorKey) {
+  this.xKey = x;
+  this.yKey = y;
+  this.radiusKey = radius;
+  this.startPointKey = startPoint;
+  this.endPointKey = endPoint;
+  this.xLeftVelocityKey = xLeftVelocity;
+  this.xRightVelocityKey = xRightVelocity;
+  this.colorKey = color;
+
+
+  this.drawAnimation = function () {
+    canvasContext.beginPath();
+    canvasContext.arc(this.xKey, this.yKey, this.radiusKey, this.startPointKey, this.endPointKey, this.xRightVelocityKey, this.xLeftVelocityKey, this.colorKey, false);
+    canvasContext.strokeStyle = this.colorKey;
+    canvasContext.stroke();
+  };
+
+  this.updatedAnimation = function () {
+    this.xKey += this.xRightVelocityKey;
+    this.xKey -= this.xLeftVelocityKey;
+    this.drawAnimation();
+  };
+}
+
+// Create an Element that will get all the Parameters and Characteristics from the Main Object.
+const newCharacter = new MainCharacter(this.xKey, this.yKey, this.radiusKey, this.startPointKey, this.endPointKey, this.xLeftVelocityKey, this.xRightVelocityKey, this.colorKey, false);
 
 // Move function.
 // Function will create a Loop with the AnimationFrame.
 // Loop will Draw and will Clear all from the Canvas Field.
-function animateDraw (){
+function animateDraw() {
   requestAnimationFrame(animateDraw);
-  canvasContext.clearRect(0, 0, innerWidth, innerHeight);
-  canvasContext.beginPath();
-  canvasContext.arc(x, y, radius, startPoint, endPoint, false);
-  canvasContext.strokeStyle = 'red';
-  canvasContext.stroke();
+  canvasContext.clearRect(0, 0, canvasArea.width, canvasArea.height);
 
-  x += xVelocity;
-
+  newCharacter.updatedAnimation();
 }
 
 // Call the AnimatedDraw Function.
