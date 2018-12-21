f64b86ba4185accab8acd82a177eff06f5c618fe
cristianpipe <cristianpipe0494@gmail.com>
2018-12-21 14:53:40 -0600

    Have added svgs files, added instructions and Btn start game has functionality

--- src/js/mainCharacter.js
+++ src/js/mainCharacter.js
@@ -4,31 +4,36 @@ const timerBtn = document.getElementById('timerBtn');
 let timer = document.getElementById('timer');
 const leftBtn = document.getElementById('leftBtn');
 const rightBtn = document.getElementById('rightBtn');
+const instructionsBox = document.getElementById('instructionsBox');
+const lifeBox = document.getElementById('lifeBox');
 
 // Set the Canvas Width and Height as the Window.
 canvasArea.width = window.innerWidth;
 canvasArea.height = window.innerHeight + 20;
 
+// Global variables and global functions.
 let timerBtnState = false;
 let startSeconds = 0;
 let rotationLevelRotated = 10;
 timer.innerHTML = 'Start game';
 
+// Function for Collisions, has parameters.
+function collisionDetection (xMainChar, yMainChar, xEnemyChar, yEnemyChar) {
+  let xDistance = xEnemyChar - xMainChar;
+  let yDistance = yEnemyChar - yMainChar;
+  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
 
-timerBtn.addEventListener('click', function () {
-  if (timerBtnState === false) {
-    timer.innerHTML = '';
-    timer.innerHTML = startSeconds;
-    console.log('stoped');
-    timerBtnState = true;
-  } else if (timerBtnState === true) {
-    do {
-      startSeconds += startSeconds + 1;
-      timer.innerHTML = startSeconds;
-    } while (timerBtnState === true);
+// Function will generate random values.
+// Use as Parameter a MaxValue.
+function generateRandomValue (maxValue) {
+  let maxNumb = maxValue;
+  let randomNumb = parseInt(Math.random() * maxNumb);
+  if (randomNumb < 3) {
+    randomNumb += 3
   }
-  timerBtnState = false;
-});
+  return randomNumb;
+}
 
 // Create a context for the Canvas Field.
 // Canvas context is 2d.
@@ -81,7 +86,7 @@ function MainCharacter(xKey, yKey, radiusKey, startPointKey, endPointKey, xLeftV
 const newCharacter = new MainCharacter(x, y, radius, startPoint, endPoint, xLeftVelocity, xRightVelocity, color, false);
 
 // Enemy Character.
-// Variables for the Character.
+// Variables for the Enemy Character.
 const yEnemy = canvasArea.height;
 const colorEnemy = 'blue';
 const gravityValue = 6;
@@ -89,16 +94,6 @@ const radiusValue = 30;
 const xEnemy = generateRandomValue(canvasArea.width);
 const gravity = generateRandomValue(gravityValue);
 const radiusEnemy = generateRandomValue(radiusValue);
-
-function generateRandomValue (maxValue) {
-  let maxNumb = maxValue;
-  let randomNumb = parseInt(Math.random() * maxNumb);
-  if (randomNumb < 3) {
-    randomNumb += 3
-  }
-  return randomNumb;
-}
-
 
 // Main Character, this Character is an object with 'Caracteristics' or 'Parameters'
 // The Parameters will be saved on its respective 'Key' or 'Variable'.
@@ -138,29 +133,37 @@ let arrayEnemies = [];
 // Inside this function is the random value generator.
 // After generating random values for each element, function will push element into the Array.
 // Every given time function will increase time by one.
-setInterval(function (){
-  const xEnemy = generateRandomValue(canvasArea.width);
-  const gravity = generateRandomValue(gravityValue);
-  const radiusEnemy = generateRandomValue(radiusValue);
-  arrayEnemies.push(new EnemyCharacter(xEnemy, yEnemy, radiusEnemy, startPoint, endPoint, colorEnemy, gravity, false));
-  startSeconds += 1;
-  timer.innerHTML = startSeconds;
-  console.log(arrayEnemies);
-  if (startSeconds === rotationLevelRotated) {
-    console.log('reached level');
-  }
-}, 5000);
-
+function initGenerateEnemies () {
+  setInterval(function (){
+    const xEnemy = generateRandomValue(canvasArea.width);
+    const gravity = generateRandomValue(gravityValue);
+    const radiusEnemy = generateRandomValue(radiusValue);
+    arrayEnemies.push(new EnemyCharacter(xEnemy, yEnemy, radiusEnemy, startPoint, endPoint, colorEnemy, gravity, false));
+    startSeconds += 1;
+    timer.innerHTML = startSeconds;
+    console.log(arrayEnemies);
+    if (startSeconds === rotationLevelRotated) {
+      console.log('reached level');
+    }
+  }, 5000);
+}
 
 // Move function.
 // Function will create a Loop with the AnimationFrame.
 // Loop will Draw and will Clear all from the Canvas Field.
 // Loop is running every frame and will check for collisions.
 function animateDraw() {
+
+  // Call function into AnimationFrame to create a loop.
   requestAnimationFrame(animateDraw);
+
+  // ClearRect will clear strokes from animation moves.
   canvasContext.clearRect(0, 0, canvasArea.width, canvasArea.height);
+
+  // Every frame will update animation from main character.
   newCharacter.updatedAnimationMainChar();
 
+  // As soon as MainCharacter reaches the edges of the screen, it'll be pushed to the opposite side.
   if (newCharacter.xKey + newCharacter.radiusKey > window.innerWidth - newCharacter.radiusKey) {
     newCharacter.xRightVelocityKey = 0;
     newCharacter.xLeftVelocityKey = 5;
@@ -169,12 +172,16 @@ function animateDraw() {
     newCharacter.xLeftVelocityKey = 0;
   }
 
+  // Every frame will update animation from Enemy Character.
+  // And it will check if there is any collision using CollisionDetection function that lifes outside main function.
   arrayEnemies.forEach(element => {
     element.updatedAnimationEnemy();
-    if (collisionDetection(newCharacter.xKey, newCharacter.yKey, element.xEnemyKey, element.yEnemyKey) < element.radiusKey + element.radiusKey) {
+    if (collisionDetection(newCharacter.xKey, newCharacter.yKey, element.xEnemyKey, element.yEnemyKey) < newCharacter.radiusKey + element.radiusKey) {
       newCharacter.xRightVelocityKey = 0;
       newCharacter.xLeftVelocityKey = 0;
       element.gravityKey = 0;
+      arrayEnemies.splice(element, 1);
+      newCharacter.radiusKey += 5;
     } else {
       element.gravityKey = generateRandomValue(gravityValue);
     }
@@ -184,23 +191,30 @@ function animateDraw() {
   });
 }
 
-// Function for Collisions, has parameters.
-function collisionDetection (xMainChar, yMainChar, xEnemyChar, yEnemyChar) {
-  let xDistance = xEnemyChar - xMainChar;
-  let yDistance = yEnemyChar - yMainChar;
-  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
-}
-
 // Call the AnimatedDraw Function.
 animateDraw();
 
+// Function will init enemies generation and will start timer.
+timerBtn.addEventListener('click', function () {
+  instructionsBox.setAttribute('class', 'hiddenInformation');
+  lifeBox.setAttribute('class', 'hiddenInformation');
+  timer.innerHTML = '';
+  timer.setAttribute('class', 'loadingStartsState');
+  initGenerateEnemies();
+  setInterval(function (){
+    if (startSeconds > 0) {
+      timer.setAttribute('class', 'loadingEndsState');
+    }
+  }, 1000);
+});
+
 // Set the EventListeners to the Btns.
-leftBtn.addEventListener('click', function (event) {
+leftBtn.addEventListener('click', function () {
   newCharacter.xRightVelocityKey = 0;
   newCharacter.xLeftVelocityKey = 3;
 });
 
-rightBtn.addEventListener('click', function (event) {
+rightBtn.addEventListener('click', function () {
   newCharacter.xRightVelocityKey = 3;
   newCharacter.xLeftVelocityKey = 0;
 });
